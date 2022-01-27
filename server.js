d3f019bcbfff5c21fea68afb044a114697e9e546
Amr Monier <a.monier2107@gmail.com>
2022-01-27 21:52:45 +0200

    added import module

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-
+import sum from "./module.js";
 const app = express();
 
 app.use(cors());
 app.get("/", (req, res, next) => {
-  res.json({ msg: "Hello" });
+  res.json({ msg: "Hello" + sum(5,7) });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
