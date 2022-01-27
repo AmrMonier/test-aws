a35c530d53408a5a3b8249aff921996c89cbbd58
Amr Monier <a.monier2107@gmail.com>
2022-01-27 21:58:23 +0200

    refactored server to use classes

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-import express from "express";
+
+import express from 'express';
 import cors from "cors";
 import sum from "./module.js";
-const app = express();
 
-app.use(cors());
-app.get("/", (req, res, next) => {
-  res.json({ msg: "Hello" + sum(5,7) });
-});
+//import routes from './routes';
+
+class Server {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+  
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+  
+  routes() {
+    //this.server.use(routes);
+    this.server.get("/", (req, res, next) => {
+      res.json({ msg: "Hello" + sum(5,7) });
+    });
+  }
+}
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+let x =  new Server().server;
+x.listen(PORT, () => {
   console.log(`APP Running on ${PORT}`);
 });
